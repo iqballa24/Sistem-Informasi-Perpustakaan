b2f2e368d456ca057b0ea7871e7c992e9ec794ab
iqballa24 <iqbalnugraha347@gmail.com>
2021-10-17 10:56:41 +0700

    add filter book

--- application/controllers/Buku.php
+++ application/controllers/Buku.php
@@ -43,7 +43,8 @@ class Buku extends CI_Controller
     }
 
     //fungsi menampilkan data dalam bentuk json
-    public function datatables() {
+    public function datatables()
+    {
         //menunda loading (bisa dihapus, hanya untuk menampilkan pesan processing)
         //sleep(2);
 
@@ -58,34 +59,34 @@ class Buku extends CI_Controller
             $row   = array();
             $row[] = $no;
             $row[] = $field['judul'];
-            $row[] = '<img src="'.base_url('upload_folder/' .$field['gambar']).'" class="img-fluid" style="height:75px; width:55px;" alt="ini gambar">';
+            $row[] = '<img src="' . base_url('upload_folder/' . $field['gambar']) . '" class="img-fluid" style="height:75px; width:55px;" alt="ini gambar">';
             $row[] = $field['kategori'];
             $row[] = $field['penerbit'];
             $row[] = $field['stok_buku'];
             $row[] = '
                     <a href="#" class="btn btn-info btn-sm detail" title="Detail" data-toggle="modal" data-target="#modal-detail" 
-                        data-isbn="'.$field['ISBN'].'"
-                        data-judul="'.$field['judul'].'"
-                        data-terbit="'.$field['thn_terbit'].'" 
-                        data-gambar="'.$field['gambar'].'" 
-                        data-kategori="'.$field['kategori'].'"
-                        data-penerbit="'.$field['penerbit'].'"
-                        data-rate="'.$field['rating'].'"
-                        data-rak="'.$field['rak'].'"> 
+                        data-isbn="' . $field['ISBN'] . '"
+                        data-judul="' . $field['judul'] . '"
+                        data-terbit="' . $field['thn_terbit'] . '" 
+                        data-gambar="' . $field['gambar'] . '" 
+                        data-kategori="' . $field['kategori'] . '"
+                        data-penerbit="' . $field['penerbit'] . '"
+                        data-rate="' . $field['rating'] . '"
+                        data-rak="' . $field['rak'] . '"> 
                         <i class="fas fa-search"></i>
                      </a>
 
-                    <a href = "'.site_url('buku/update/'.$field['id_buku']). '" class = "btn btn-warning btn-sm" title = "Edit">
+                    <a href = "' . site_url('buku/update/' . $field['id_buku']) . '" class = "btn btn-warning btn-sm" title = "Edit">
                         <i class = "fas fa-edit"></i> 
                      </a>
 
-                     <a href = "'.site_url('buku/delete/'.$field['id_buku']). '" class ="btn btn-danger btn-sm btnHapus" title = "Hapus">
+                     <a href = "' . site_url('buku/delete/' . $field['id_buku']) . '" class ="btn btn-danger btn-sm btnHapus" title = "Hapus">
                         <i class = "fas fa-trash-alt"></i> 
                      </a>';
 
             $data[] = $row;
         }
-    
+
         //mengirim data json
         $output = array(
             "draw"            => $this->input->post('draw'),
@@ -105,13 +106,13 @@ class Buku extends CI_Controller
         $data_kategori = $this->kategori_model->read();
         $data_penerbit = $this->penerbit_model->read();
         $NIP = $this->session->userdata('nama');
-         
-         //mengirim data ke view
-         $output = array(
-             'judul'      => 'Tambah buku',
-             'theme_page' => 'buku_insert',
-             
-             //mengirim daftar provinsi ke view
+
+        //mengirim data ke view
+        $output = array(
+            'judul'      => 'Tambah buku',
+            'theme_page' => 'buku_insert',
+
+            //mengirim daftar provinsi ke view
             'data_petugas'  => $NIP,
             'data_kategori' => $data_kategori,
             'data_penerbit' => $data_penerbit,
@@ -133,9 +134,9 @@ class Buku extends CI_Controller
             $this->form_validation->set_rules('thnTerbit', 'Tahun Terbit', 'required|numeric');
             $this->form_validation->set_rules('stok', 'Stok', 'required|numeric');
             $this->form_validation->set_rules('rate', 'Rating', 'required');
-            
+
             //setting library upload
-            $config = array (
+            $config = array(
                 'upload_path'    => './upload_folder/',
                 'allowed_types'  => 'gif|jpg|png',
                 'max_size'       => 5000
@@ -144,7 +145,7 @@ class Buku extends CI_Controller
             $this->load->library('upload', $config);
 
             if ($this->form_validation->run() == TRUE) {
-                
+
                 //menangkap data input dari view
                 $kategori = $this->input->post('id_kategori');
                 $penerbit = $this->input->post('id_penerbit');
@@ -153,38 +154,38 @@ class Buku extends CI_Controller
                 $nama = $this->input->post('nama');
                 $stok = $this->input->post('stok');
                 $rate = $this->input->post('rate');
-        
+
                 //jika gagal upload
                 if (!$this->upload->do_upload('userfile')) {
-        
+
                     //mengambil daftar provinsi dari table provinsi
                     $data_kategori = $this->kategori_model->read();
                     $data_penerbit = $this->penerbit_model->read();
                     $NIP = $this->session->userdata('nama');
-        
+
                     //respon alasan kenapa gagal upload
                     $response = $this->upload->display_errors();
-        
+
                     //mengirim data ke view
                     $output = array(
                         'judul'      => 'Tambah buku',
                         'theme_page' => 'buku_insert',
                         'response'   => $response,
-        
+
                         //mengirim daftar provinsi ke view
                         'data_kategori' => $data_kategori,
                         'data_penerbit' => $data_penerbit,
                         'data_petugas'  => $NIP
                     );
-        
+
                     //memanggil file view
                     $this->load->view('theme/index', $output);
-        
-                //jika berhasil upload
+
+                    //jika berhasil upload
                 } else {
                     $this->upload->do_upload('userfile');
                     $upload_data = $this->upload->data('file_name');
-        
+
                     //mengirim data ke model
                     $input = array(
                         //format : nama field/kolom table => data input dari view
@@ -197,17 +198,16 @@ class Buku extends CI_Controller
                         'rating'      => $rate,
                         'gambar'      => $upload_data
                     );
-        
+
                     //memanggil function insert pada kota model
                     //function insert berfungsi menyimpan/create data ke table buku di database
                     $data_buku = $this->buku_model->insert($input);
-        
+
                     //mengembalikan halaman ke function read
                     $this->session->set_tempdata('message', 'Data berhasil ditambahkan', 1);
-                    Redirect('buku/read'); 
+                    Redirect('buku/read');
                 }
             }
-
         }
     }
 
@@ -224,9 +224,9 @@ class Buku extends CI_Controller
         if (!empty($data_user)) {
 
             //membuat pesan error
-            $this->form_validation->set_message('insert_check',"Buku dengan ISBN " . $isbn . " sudah ada", 1);
+            $this->form_validation->set_message('insert_check', "Buku dengan ISBN " . $isbn . " sudah ada", 1);
             $this->session->set_tempdata('info', "Tidak dapat memasukan data yang sama", 1);
-            
+
             return FALSE;
         }
         return TRUE;
@@ -235,25 +235,25 @@ class Buku extends CI_Controller
     public function update()
     {
         //menangkap id data yg dipilih dari view (parameter get)
-         $id = $this->uri->segment(3); 
-         $NIP = $this->session->userdata('nama');
-         
-         //function read berfungsi mengambil 1 data dari table kota sesuai id yg dipilih
-         $data_buku_single = $this->buku_model->read_single($id);
-         
-         //mengambil daftar kategori dan penerbit
-         $data_kategori = $this->kategori_model->read();
-         $data_penerbit = $this->penerbit_model->read();
-         
-         //mengirim data ke view
-         $output = array(
-             'judul'      => 'Edit Buku',
-             'theme_page' => 'buku_update',
-             
-             //mengirim data kota yang dipilih ke view
-             'data_buku_single' => $data_buku_single,
-             
-             //mengirim daftar provinsi ke view
+        $id = $this->uri->segment(3);
+        $NIP = $this->session->userdata('nama');
+
+        //function read berfungsi mengambil 1 data dari table kota sesuai id yg dipilih
+        $data_buku_single = $this->buku_model->read_single($id);
+
+        //mengambil daftar kategori dan penerbit
+        $data_kategori = $this->kategori_model->read();
+        $data_penerbit = $this->penerbit_model->read();
+
+        //mengirim data ke view
+        $output = array(
+            'judul'      => 'Edit Buku',
+            'theme_page' => 'buku_update',
+
+            //mengirim data kota yang dipilih ke view
+            'data_buku_single' => $data_buku_single,
+
+            //mengirim daftar provinsi ke view
             'data_petugas'  => $NIP,
             'data_kategori' => $data_kategori,
             'data_penerbit' => $data_penerbit,
@@ -316,7 +316,7 @@ class Buku extends CI_Controller
             //memanggil file view
             $this->load->view('theme/index', $output);
 
-         //jika berhasil upload
+            //jika berhasil upload
         } else {
             $this->upload->do_upload('userfile');
             $upload_data = $this->upload->data('file_name');
@@ -350,13 +350,11 @@ class Buku extends CI_Controller
         $id = $this->uri->segment(3);
 
         $this->db->db_debug = false; //disable debugging queries
- 
+
         // Error handling
-        if (!$this->buku_model->delete($id)) 
-        {
+        if (!$this->buku_model->delete($id)) {
             $msg =  $this->db->error();
             $this->session->set_tempdata('error', $msg['message'], 1);
-
         }
 
         //mengembalikan halaman ke function read
@@ -368,10 +366,10 @@ class Buku extends CI_Controller
     {
         $data_buku = $this->buku_model->read();
         $NIP = $this->session->userdata('nama');
-        
+
         //mengirim data ke view
         $output = array(
-            
+
             //data provinsi dikirim ke view
             'data_petugas' => $NIP,
             'data_buku' => $data_buku
@@ -379,5 +377,34 @@ class Buku extends CI_Controller
 
         //memanggil file view
         $this->load->view('buku_export', $output);
+    }
+
+    public function filterBook()
+    {
+        $NIP = $this->session->userdata('nama');
+
+        $output = array(
+            'judul'         => 'Pencarian Buku',
+            'theme_page'    => 'buku_filter',
+            'data_petugas'  => $NIP
+
+        );
+
+        $this->load->view('theme/index', $output);
+    }
+
+    public function fetchBook()
+    {
+        $query = $this->input->post('query');
+
+        if ($query != '') {
+            $qry = "SELECT * from buku a join penerbit b on a.id_penerbit = b.id_penerbit join kategori c on a.id_kategori = c.id_kategori where judul LIKE '$query%'";
+            $result = $this->db->query($qry);
+    
+            echo json_encode($result->result_array());
+        }else{
+            echo json_encode(['empty' => 'Ketik nama buku yang ingin dicari']);
+			die();
+        }
     }
 }
--- application/views/anggota_read.php
+++ application/views/anggota_read.php
@@ -1,7 +1,7 @@
 <!-- Body Content -->
 <div class="card shadow mb-4">
 	<div class="card-header">
-		<a href="<?php echo site_url('anggota/insert') ?>"><i class="fas fa-plus"></i>
+		<a href="#" data-toggle="modal" data-target="#Modal_Add"><i class="fas fa-plus"></i>
 			Add New</a>
 		<a href="<?php echo site_url('anggota/import') ?>"><i class="fas fa-plus"></i>
 			Import</a>
@@ -29,10 +29,102 @@
 </div>
 <!-- End Body Content -->
 
+<!-- Modal Add -->
+<form>
+	<div class="modal fade" id="Modal_Add" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+		<div class="modal-dialog modal-lg" role="document">
+			<div class="modal-content">
+				<div class="modal-header">
+					<h5 class="modal-title" id="exampleModalLabel">Add New Product</h5>
+					<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+						<span aria-hidden="true">&times;</span>
+					</button>
+				</div>
+				<div class="modal-body">
+					<div class="form-group row">
+						<label class="col-md-2 col-form-label">NIM</label>
+						<div class="col-md-10">
+							<input type="text" name="nim" id="nim" class="form-control" placeholder="NIM">
+						</div>
+					</div>
+					<div class="form-group row">
+						<label class="col-md-2 col-form-label">Nama</label>
+						<div class="col-md-10">
+							<input type="text" name="nama" id="nama" class="form-control" placeholder="Nama">
+						</div>
+					</div>
+					<div class="form-group row">
+						<label class="col-md-2 col-form-label">Program Studi</label>
+						<div class="col-md-10">
+							<input type="text" name="program_studi" id="program_studi" class="form-control" placeholder="Program Studi">
+						</div>
+					</div>
+					<div class="form-group row">
+						<label class="col-md-2 col-form-label">Email</label>
+						<div class="col-md-10">
+							<input type="text" name="email" id="email" class="form-control" placeholder="Email">
+						</div>
+					</div>
+				</div>
+				<div class="modal-footer">
+					<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+					<button type="button" type="submit" id="btn_save" class="btn btn-primary">Save</button>
+				</div>
+			</div>
+		</div>
+	</div>
+</form>
+<!--END MODAL ADD-->
+
+<!-- MODAL UPDATE -->
+<div class="modal fade" id="Modal_Update" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+	<div class="modal-dialog" role="document">
+		<div class="modal-content">
+			<div class="modal-header">
+				<h4 class="modal-title" id="myModalLabel">Update Data</h4>
+			</div>
+			<div class="modal-body">
+				<form>
+					<div class="form-group row">
+						<label class="col-md-2 col-form-label">NIM</label>
+						<div class="col-md-10">
+							<input type="text" name="nim_u" id="nim_u" class="form-control" placeholder="NIM">
+						</div>
+					</div>
+					<div class="form-group row">
+						<label class="col-md-2 col-form-label">Nama</label>
+						<div class="col-md-10">
+							<input type="text" name="nama_u" id="nama_u" class="form-control" placeholder="Nama">
+						</div>
+					</div>
+					<div class="form-group row">
+						<label class="col-md-2 col-form-label">Program Studi</label>
+						<div class="col-md-10">
+							<input type="text" name="program_studi_u" id="program_studi_u" class="form-control" placeholder="Program Studi">
+						</div>
+					</div>
+					<div class="form-group row">
+						<label class="col-md-2 col-form-label">Email</label>
+						<div class="col-md-10">
+							<input type="text" name="email_u" id="email_u" class="form-control" placeholder="Email">
+						</div>
+						<input type="hidden" id="id_anggota" name="id_anggota">
+					</div>
+					<div class="modal-footer">
+						<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+						<button type="button" type="submit" id="btn_update" class="btn btn-primary">Update</button>
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+</div>
+<!-- END MODAL UPDATE -->
+
 <!-- Js -->
 <script type="text/javascript">
-	$(document).ready(() => {
-		$('#table').DataTable({
+	document.addEventListener("DOMContentLoaded", function() {
+		var table = $('#table').DataTable({
 			"responsive": true,
 			"processing": true,
 			"language": {
@@ -50,6 +142,130 @@
 				"orderable": false,
 			}],
 		});
+
+		//Save anggota
+		$('#btn_save').on('click', function() {
+			let nim = $('#nim').val();
+			let nama = $('#nama').val();
+			let program_studi = $('#program_studi').val();
+			let email = $('#email').val();
+
+			$.ajax({
+				type: "POST",
+				url: "<?php echo site_url('anggota/save') ?>",
+				dataType: "JSON",
+				data: {
+					nim: nim,
+					nama: nama,
+					program_studi: program_studi,
+					email: email
+				},
+				success: function(data) {
+					if (data.error) {
+						console.log(data.error)
+						Swal.fire({
+							icon: 'error',
+							title: 'Oops...',
+							text: data.error,
+						})
+					}
+
+					if (data.success) {
+						$('[name="nim"]').val("");
+						$('[name="nama"]').val("");
+						$('[name="program_studi"]').val("");
+						$('[name="email"]').val("");
+						$('#Modal_Add').modal('hide');
+						Swal.fire({
+							title: 'Success',
+							text: data.success,
+							icon: 'success'
+						});
+						table.ajax.reload();
+					}
+				},
+				error: function(request, status, error) {
+					alert(error);
+				}
+			});
+			// table.ajax.reload();
+			// return false;
+		});
+
+		//GET data update anggota
+		$('#table tbody').on('click', '.item_update', function(e) {
+			e.preventDefault();
+
+			const id_anggota = $(this).attr('data');
+
+			$.ajax({
+				type: "GET",
+				url: `<?php echo base_url() ?>index.php/anggota/get_update/${id_anggota}`,
+				dataType: "JSON",
+				data: {
+					id_anggota: id_anggota
+				},
+				success: function(data) {
+					$('#Modal_Update').modal('show');
+					$.each(data, function() {
+						$('[name="nim_u"]').val(data.nim);
+						$('[name="nama_u"]').val(data.nama);
+						$('[name="program_studi_u"]').val(data.prodi);
+						$('[name="email_u"]').val(data.email);
+						$('#id_anggota').val(data.id_anggota);
+					});
+				},
+				error: function(request, status, error) {
+					alert(request.responseText);
+				}
+			});
+		});
+
+		// SIMPAN ANGGOTA
+		$('#btn_update').on('click', function() {
+			let nim = $('[name="nim_u"]').val();
+			let nama = $('[name="nama_u"]').val();
+			let program_studi = $('[name="program_studi_u"]').val();
+			let email = $('[name="email_u"]').val();
+			let id_anggota = $('#id_anggota').val();
+
+			$.ajax({
+				type: "POST",
+				url: "<?php echo site_url('anggota/Simpan') ?>",
+				dataType: "JSON",
+				data: {
+					nim_u: nim,
+					nama_u: nama,
+					program_studi_u: program_studi,
+					email_u: email,
+					id_anggota: id_anggota
+				},
+				success: function(data) {
+					if (data.error) {
+						console.log(data.error)
+						Swal.fire({
+							icon: 'error',
+							title: 'Oops...',
+							text: data.error,
+						})
+					}
+
+					if (data.success) {
+						$('#Modal_Update').modal('hide');
+						Swal.fire({
+							title: 'Success',
+							text: data.success,
+							icon: 'success'
+						});
+						table.ajax.reload();
+					}
+				},
+				error: function(request, status, error) {
+					alert(error);
+				}
+			});
+		});
+
 
 	});
 </script>
